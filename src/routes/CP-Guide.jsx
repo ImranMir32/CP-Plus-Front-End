021fa9a0b194498115a69525b38b6b1c34e2129a
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-15 02:13:14 +0600

    adding scrollview on cp-guide

--- src/routes/CP-Guide.jsx
+++ src/routes/CP-Guide.jsx
@@ -29,170 +29,270 @@ const CpGuide = () => {
       <div className="cp-container">
         <div className="cp-topic-name">
           <div className="cp-content">
-            <h3>Beginner section</h3>
-            <p
-              onClick={() => {
-                handleButtonClick("Programming Languages");
-              }}
-              className={
-                Page === "Programming Languages" ? "cp-p-active" : "cp-p"
-              }
-            >
-              Programming Languages
-            </p>
+            <div className="scrollable-section">
+              <h3>Beginner section</h3>
+              <p
+                onClick={() => {
+                  handleButtonClick("Programming Languages");
+                }}
+                className={
+                  Page === "Programming Languages" ? "cp-p-active" : "cp-p"
+                }
+              >
+                Programming Languages
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("Data Types and Variables");
-              }}
-              className={
-                Page === "Data Types and Variables" ? "cp-p-active" : "cp-p"
-              }
-            >
-              Data Types and Variables
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("Data Types and Variables");
+                }}
+                className={
+                  Page === "Data Types and Variables" ? "cp-p-active" : "cp-p"
+                }
+              >
+                Data Types and Variables
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("Operators and Expressions");
-              }}
-              className={
-                Page === "Operators and Expressions" ? "cp-p-active" : "cp-p"
-              }
-            >
-              Operators and Expressions
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("Operators and Expressions");
+                }}
+                className={
+                  Page === "Operators and Expressions" ? "cp-p-active" : "cp-p"
+                }
+              >
+                Operators and Expressions
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("Input/Output");
-              }}
-              className={Page === "Input/Output" ? "cp-p-active" : "cp-p"}
-            >
-              Input/Output
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("Input/Output");
+                }}
+                className={Page === "Input/Output" ? "cp-p-active" : "cp-p"}
+              >
+                Input/Output
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("If Else");
-              }}
-              className={Page === "If Else" ? "cp-p-active" : "cp-p"}
-            >
-              If Else
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("If Else");
+                }}
+                className={Page === "If Else" ? "cp-p-active" : "cp-p"}
+              >
+                If Else
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("Loops");
-              }}
-              className={Page === "Loops" ? "cp-p-active" : "cp-p"}
-            >
-              Loops
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("Loops");
+                }}
+                className={Page === "Loops" ? "cp-p-active" : "cp-p"}
+              >
+                Loops
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("Array");
-              }}
-              className={Page === "Array" ? "cp-p-active" : "cp-p"}
-            >
-              Array
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("Array");
+                }}
+                className={Page === "Array" ? "cp-p-active" : "cp-p"}
+              >
+                Array
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("String");
-              }}
-              className={Page === "String" ? "cp-p-active" : "cp-p"}
-            >
-              String
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("String");
+                }}
+                className={Page === "String" ? "cp-p-active" : "cp-p"}
+              >
+                String
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("Function");
-              }}
-              className={Page === "Function" ? "cp-p-active" : "cp-p"}
-            >
-              Function
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("Function");
+                }}
+                className={Page === "Function" ? "cp-p-active" : "cp-p"}
+              >
+                Function
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("Recursion");
-              }}
-              className={Page === "Recursion" ? "cp-p-active" : "cp-p"}
-            >
-              Recursion
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("Recursion");
+                }}
+                className={Page === "Recursion" ? "cp-p-active" : "cp-p"}
+              >
+                Recursion
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("Time complexity and Big O notation");
-              }}
-              className={
-                Page === "Time complexity and Big O notation"
-                  ? "cp-p-active"
-                  : "cp-p"
-              }
-            >
-              Time complexity and Big O notation
-            </p>
+              <p
+                onClick={() => {
+                  handleButtonClick("Time complexity and Big O notation");
+                }}
+                className={
+                  Page === "Time complexity and Big O notation"
+                    ? "cp-p-active"
+                    : "cp-p"
+                }
+              >
+                Time complexity and Big O notation
+              </p>
 
-            <p
-              onClick={() => {
-                handleButtonClick("Bit manipulation");
-              }}
-              className={Page === "Bit manipulation" ? "cp-p-active" : "cp-p"}
-            >
-              Bit manipulation
-            </p>
-          </div>
-          <div className="cp-content">
-            <h3>Searching and Sorting Algorithms</h3>
-          </div>
-          <div className="intermediate">
-            <h3>Intermediate section</h3>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-          </div>
-          <div className="advanced">
-            <h3>Advanced section</h3>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
-            <p>contant onk import</p>
+              <p
+                onClick={() => {
+                  handleButtonClick("Bit manipulation");
+                }}
+                className={Page === "Bit manipulation" ? "cp-p-active" : "cp-p"}
+              >
+                Bit manipulation
+              </p>
+
+              <div className="cp-content">
+                <h3>Searching and Sorting Algorithms</h3>
+              </div>
+              <div className="intermediate">
+                <h3>Intermediate section</h3>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+              </div>
+              <div className="advanced">
+                <h3>Advanced section</h3>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+              </div>
+
+              <div className="cp-content">
+                <h3>Searching and Sorting Algorithms</h3>
+              </div>
+              <div className="intermediate">
+                <h3>Intermediate section</h3>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+              </div>
+              <div className="advanced">
+                <h3>Advanced section</h3>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+              </div>
+
+              <div className="cp-content">
+                <h3>Searching and Sorting Algorithms</h3>
+              </div>
+              <div className="intermediate">
+                <h3>Intermediate section</h3>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+              </div>
+              <div className="advanced">
+                <h3>Advanced section</h3>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+              </div>
+
+              <div className="cp-content">
+                <h3>Searching and Sorting Algorithms</h3>
+              </div>
+              <div className="intermediate">
+                <h3>Intermediate section</h3>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+              </div>
+              <div className="advanced">
+                <h3>Advanced section</h3>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+                <p>contant onk import</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="vertical-line"></div>
-        <div className="cp-topic-details">
-          <h1>{Page}</h1>
-          {Page === "Programming Languages" && <ProgrammingLanguages />}
-          {Page === "Data Types and Variables" && <DataTypes />}
-          {Page === "Operators and Expressions" && <OperatorsExpressions />}
-          {Page === "If Else" && <IfElse />}
-          {Page === "Input/Output" && <InputOutput />}
-          {Page === "Loops" && <LoopInfo />}
-          {Page === "Array" && <ArrayInfo />}
-          {Page === "String" && <StringInfo />}
-          {Page === "Function" && <FunctionInfo />}
-          {Page === "Recursion" && <RecursionInfo />}
-          {Page === "Time complexity and Big O notation" && <TimeComplexity />}
-          {Page === "Bit manipulation" && <BitManipulation />}
+        <div className="scrollable-section">
+          <div className="cp-topic-and-ad">
+            <div className="cp-topic-details">
+              <h1>{Page}</h1>
+              {Page === "Programming Languages" && <ProgrammingLanguages />}
+              {Page === "Data Types and Variables" && <DataTypes />}
+              {Page === "Operators and Expressions" && <OperatorsExpressions />}
+              {Page === "If Else" && <IfElse />}
+              {Page === "Input/Output" && <InputOutput />}
+              {Page === "Loops" && <LoopInfo />}
+              {Page === "Array" && <ArrayInfo />}
+              {Page === "String" && <StringInfo />}
+              {Page === "Function" && <FunctionInfo />}
+              {Page === "Recursion" && <RecursionInfo />}
+              {Page === "Time complexity and Big O notation" && (
+                <TimeComplexity />
+              )}
+              {Page === "Bit manipulation" && <BitManipulation />}
+            </div>
+            <div className="ad"></div>
+          </div>
         </div>
       </div>
       <Footer />
