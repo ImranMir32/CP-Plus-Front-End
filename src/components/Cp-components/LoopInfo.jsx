ec244e2c2d031029958e575fbcc068f59326aa90
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-14 21:17:12 +0600

    adding problem on loop page

--- src/components/Cp-components/LoopInfo.jsx
+++ src/components/Cp-components/LoopInfo.jsx
@@ -61,6 +61,160 @@ const LoopInfo = () => {
             >
               https://www.youtube.com/watch?v=jdg-W5Y4TTg
             </a>
+
+            <p>Problem and Solution Links :</p>
+            <ul>
+              <li>
+                Write a program in C++ to find the first 10 natural numbers
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://www.w3resource.com/cpp-exercises/for-loop/cpp-for-loop-exercise-1.php"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://www.w3resource.com/cpp-exercises/for-loop/cpp-for-loop-exercise-1.php
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a program in C++ to find the sum of the first 10 natural
+                numbers.
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://www.w3resource.com/cpp-exercises/for-loop/cpp-for-loop-exercise-2.php"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://www.w3resource.com/cpp-exercises/for-loop/cpp-for-loop-exercise-2.php
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a program in C++ to check whether a number is prime or
+                not.
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://www.w3resource.com/cpp-exercises/for-loop/cpp-for-loop-exercise-5.php"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://www.w3resource.com/cpp-exercises/for-loop/cpp-for-loop-exercise-5.php
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a program in C++ to find a prime number within a range.
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://pastebin.ubuntu.com/p/Z326v2FWvh/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://pastebin.ubuntu.com/p/Z326v2FWvh/
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a program in C++ to find the factorial of a number.
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://pastebin.ubuntu.com/p/N2sfCXH5n6/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://pastebin.ubuntu.com/p/N2sfCXH5n6/
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a program in C++ to find the last prime number that occurs
+                before the entered number.
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://pastebin.ubuntu.com/p/wXQGGS2pMR/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://pastebin.ubuntu.com/p/wXQGGS2pMR/
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a program in C++ to find the Greatest Common Divisor (GCD)
+                of two numbers.
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://pastebin.ubuntu.com/p/Js9wqSQGmg/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://pastebin.ubuntu.com/p/Js9wqSQGmg/
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a program in C++ to find the sum of the digits of a given
+                number.
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://pastebin.ubuntu.com/p/r66TWtv6fT/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://pastebin.ubuntu.com/p/r66TWtv6fT/
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a program to calculate profit or loss.
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://pastebin.ubuntu.com/p/QFcfmpds7n/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://pastebin.ubuntu.com/p/QFcfmpds7n/
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a program in C++ to calculate the series (1) + (1+2) +
+                (1+2+3) + (1+2+3+4) + ... + (1+2+3+4+...+n).
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://pastebin.ubuntu.com/p/MCkmq2rppF/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://pastebin.ubuntu.com/p/MCkmq2rppF/
+                  </a>
+                </p>
+              </li>
+              <li>
+                Write a C++ program to list non-prime numbers from 1 to an
+                upperbound.
+                <p>
+                  Solution :{" "}
+                  <a
+                    href="https://pastebin.ubuntu.com/p/mnJvFC5ZDg/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    https://pastebin.ubuntu.com/p/mnJvFC5ZDg/
+                  </a>
+                </p>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
